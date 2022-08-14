b603acb0c1d622dbbbcfe6b08f599de467d932cc
Charlie Say <charliewsay@gmail.com>
2022-08-14 15:01:58 +0100

    :lipstick: Added simple footer

--- src/components/layout/wrapper.tsx
+++ src/components/layout/wrapper.tsx
@@ -21,7 +21,7 @@ export const Wrapper = (props: { children: React.ReactNode }) => {
               <Link href="/collections">
                 <button
                   type="button"
-                  className="text-sm text-primary-ctaText decocration-primary-ctaBackground underline underline-offset-1 font-medium px-3 py-2 mr-2 mb-2"
+                  className="text-sm text-primary-ctaText decocration-primary-ctaBackground underline underline-offset-1 font-medium px-3 py-2 mr-2 mb-2 hover:cursor-pointer hover:text-primary-highlight"
                 >
                   Collections
                 </button>
@@ -33,6 +33,32 @@ export const Wrapper = (props: { children: React.ReactNode }) => {
       <div className="container mx-auto p-4 max-w-screen-lg">
         {props.children}
       </div>
+      <footer className="container mx-auto p-4 max-w-screen-lg">
+        <p className="text-primary-text text-xs">100 Projects by 100DEV</p>
+        <p className="text-primary-text text-xs">
+          Made in{' '}
+          <a
+            className="text-xs hover:cursor-pointer hover:text-primary-highlight"
+            href="https://nextjs.org/"
+          >
+            {' '}
+            NextJS
+          </a>
+          , hosted by{' '}
+          <a
+            className="text-xs hover:cursor-pointer hover:text-primary-highlight"
+            href="https://vercel.com"
+          >
+            Vercel.
+          </a>{' '}
+          <a
+            className="text-xs hover:cursor-pointer hover:text-primary-highlight"
+            href="//github.com/charlieSay/100-projects"
+          >
+            (GitHub Repo)
+          </a>
+        </p>
+      </footer>
     </main>
   )
 }
