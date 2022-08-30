9bf033d397fc6b7551e347e9a9d7071c91b5c436
Charlie Say <charliewsay@gmail.com>
2022-08-30 17:24:26 +0100

    :recycle: Refactoring

--- src/components/layout/wrapper.tsx
+++ src/components/layout/wrapper.tsx
@@ -3,63 +3,59 @@ import Link from 'next/link'
 
 export const Wrapper = (props: { children: React.ReactNode }) => {
   return (
-    <main className="container mx-auto py-4 max-w-screen-lg">
+    <main className="container mx-auto p-4 max-w-screen-lg">
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
       </Head>
-      <header role="navigation" aria-label="Main">
-        <nav className="max-w-screen-lg mx-auto pt-2 pb-4">
-          <div className="container mx-auto flex justify-between w-full">
-            <div className="flex items-center hover:cursor-pointer">
-              <Link href="/">
-                <p className="font-semibold text-xl text-secondary-text">
-                  100.DEV
-                </p>
+      <section>
+        <header role="navigation" aria-label="Main">
+          <nav className="max-w-screen-lg mx-auto pt-2 pb-4">
+            <div className="container mx-auto flex justify-between w-full">
+              <Link
+                href="/"
+                className="font-semibold text-xl text-secondary-text hover:cursor-pointer"
+              >
+                100.DEV
               </Link>
-            </div>
-            <div>
               <Link href="/popular-collections">
-                <button
-                  type="button"
-                  className="text-sm text-primary-ctaText decocration-primary-ctaBackground underline underline-offset-1 font-medium hover:cursor-pointer hover:text-primary-highlight"
-                >
+                <a className="text-sm text-primary-ctaText decocration-primary-ctaBackground underline underline-offset-1 font-medium hover:cursor-pointer hover:text-primary-highlight">
                   Popular
-                </button>
+                </a>
               </Link>
             </div>
-          </div>
-        </nav>
-      </header>
-      <section>{props.children}</section>
-      <footer className="pt-8 py-4">
-        <p className="text-primary-text text-xs">
-          100 Projects by{' '}
-          <span className="text-primary-text font-bold text-xs">100DEV</span>
-        </p>
-        <p className="text-primary-text text-xs">
-          Made in{' '}
-          <a
-            className="text-xs hover:cursor-pointer hover:text-primary-highlight"
-            href="https://nextjs.org/"
-          >
-            {' '}
-            NextJS
-          </a>
-          , hosted by{' '}
-          <a
-            className="text-xs hover:cursor-pointer hover:text-primary-highlight"
-            href="https://vercel.com"
-          >
-            Vercel.
-          </a>{' '}
-          <a
-            className="text-xs hover:cursor-pointer hover:text-primary-highlight"
-            href="//github.com/charlieSay/100-projects"
-          >
-            (GitHub Repo)
-          </a>
-        </p>
-      </footer>
+          </nav>
+        </header>
+        <main>{props.children}</main>
+        <footer className="pt-8 py-4">
+          <p className="text-primary-text text-xs">
+            100 Projects by{' '}
+            <span className="text-primary-text font-bold text-xs">100DEV</span>
+          </p>
+          <p className="text-primary-text text-xs">
+            Made in{' '}
+            <a
+              className="text-xs hover:cursor-pointer hover:text-primary-highlight"
+              href="https://nextjs.org/"
+            >
+              {' '}
+              NextJS
+            </a>
+            , hosted by{' '}
+            <a
+              className="text-xs hover:cursor-pointer hover:text-primary-highlight"
+              href="https://vercel.com"
+            >
+              Vercel.
+            </a>{' '}
+            <a
+              className="text-xs hover:cursor-pointer hover:text-primary-highlight"
+              href="//github.com/charlieSay/100-projects"
+            >
+              (GitHub Repo)
+            </a>
+          </p>
+        </footer>
+      </section>
     </main>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import type { GetStaticProps, NextPage } from 'next'
-import Head from 'next/head'
 import { getPopularProjects } from '../api/get-posts'
 import {
   CollectionHeroGroup,
