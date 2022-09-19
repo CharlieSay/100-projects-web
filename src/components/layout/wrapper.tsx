d7c3ebf9d400bb5af917a7a8d9e016cd05fe039a
Charlie Say <charliewsay@gmail.com>
2022-09-19 18:06:46 +0100

    :mag: Attempting to approve SEo

--- src/components/layout/wrapper.tsx
+++ src/components/layout/wrapper.tsx
@@ -7,6 +7,19 @@ export const Wrapper = (props: { children: React.ReactNode }) => {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <title>100 Projects</title>
+        <meta name="title" content="100 Projects for developers to do" />
+        <meta
+          name="description"
+          content="A library of projects for developers if they're stuck on what to do next. 100 Projects will give you inspiration of what project to do next to upskill yourself"
+        />
+        <meta
+          name="keywords"
+          content="software, web design, websites, javascript, java"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="charlie@talesoft.digital" />
       </Head>
       <section>
         <header role="navigation" aria-label="Main">
